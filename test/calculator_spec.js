ee2006875ada671c3798a8e87486041e9abc7321
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-19 17:19:28 -1000

    added memory tests

--- test/calculator_spec.js
+++ test/calculator_spec.js
@@ -47,8 +47,82 @@ describe('calculator module', function() {
 
 describe('calculator functions', function() {
 
-  it('should add two numbers', function() {
-
+  beforeEach(function() {
+    newCalc = calculator();
   });
 
+  it('should add two numbers', function() {
+    newCalc.load(4);
+    newCalc.add(5);
+    expect(newCalc.getTotal()).to.equal(9);
+    newCalc.add(3);
+    expect(newCalc.getTotal()).to.equal(12);
+  });
+
+  it('should subtract two numbers', function() {
+    newCalc.load(4);
+    newCalc.subtract(3);
+    expect(newCalc.getTotal()).to.equal(1);
+    newCalc.subtract(3);
+    expect(newCalc.getTotal()).to.equal(-2);
+  });
+
+  it('should multiply two numbers', function() {
+    newCalc.load(4);
+    newCalc.multiply(3);
+    expect(newCalc.getTotal()).to.equal(12);
+    newCalc.multiply(-2);
+    expect(newCalc.getTotal()).to.equal(-24);
+  });
+
+  it('should divide two numbers', function() {
+    newCalc.load(8);
+    newCalc.divide(2);
+    expect(newCalc.getTotal()).to.equal(4);
+    newCalc.divide(0);
+    expect(newCalc.getTotal()).to.equal(Infinity);
+  });
+
+  it('should not accept non-number arguments', function() {
+    expect(newCalc.add.bind(null, 'cat')).to.throw(TypeError);
+    expect(newCalc.subtract.bind(null, undefined)).to.throw(TypeError);
+    expect(newCalc.multiply.bind(null, [3])).to.throw(TypeError);
+    expect(newCalc.divide.bind(null, { value: 2})).to.throw(TypeError);
+  });
+
+});
+
+describe('memory functions', function(){
+  beforeEach(function() {
+    newCalc = calculator();
+  });
+
+  it('should have a private variable for memory', function() {
+    expect(newCalc.memory).to.be.undefined;
+  });
+
+  it('should have a method called `recallMemory` which returns its memory value', function() {
+    expect(newCalc.recallMemory).to.exist;
+    expect(newCalc.recallMemory).to.be.a.function;
+    expect(newCalc.recallMemory()).to.be.a.number;
+  });
+
+  it('should have a method to save an internal value to memory', function() {
+    expect(newCalc.saveMemory).to.exist;
+    expect(newCalc.saveMemory).to.be.a.function;
+    expect(newCalc.saveMemory).to.not.have.arguments;
+    expect(newCalc.saveMemory()).to.be.undefined;
+    newCalc.load(4);
+    newCalc.saveMemory();
+    expect(newCalc.recallMemory()).to.be.equal(4);
+  });
+
+  it('should have a method to clear memory', function() {
+    expect(newCalc.clearMemory).to.exist;
+    expect(newCalc.clearMemory).to.be.a.function;
+    newCalc.load(4);
+    newCalc.saveMemory();
+    newCalc.clearMemory();
+    expect(newCalc.recallMemory()).to.be.equal(0);
+  });
 });
